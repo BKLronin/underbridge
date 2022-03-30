41408de31c2a6e41f383bbfe8bf090202cb27b34
Thomas Herrmann <therrmann23@gmail.com>
2022-03-30 21:27:51 +0200

    Windows executable! and Device List for Audio

--- underbridge.py
+++ underbridge.py
@@ -38,6 +38,13 @@ def getMIDIDevice():
     except:
         displaymsg.set("Can´t find OP-Z : MIDI Error")
 
+def getAudioDevice():
+    p = pyaudio.PyAudio()
+    info = p.get_host_api_info_by_index(0)
+    numdevices = info.get('deviceCount')
+    for i in range(0, numdevices):
+        if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+            print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
 
 def getBPM():
     global op_device
@@ -204,6 +211,7 @@ def sequenceMaster():
 
     getMIDIDevice()
     time.sleep(1)
+    getAudioDevice()
 
     displaymsg.set("Sequence started")
 
@@ -238,7 +246,7 @@ def sequenceMaster():
                     pattern_nr = 0
                 sequenceMaster()
     except:
-        displaymsg.set("OP-Z connection Problem :(")
+        displaymsg.set("OP-Z Audio Device Problem :(")
 
 def cancelRec():
     global cancel
